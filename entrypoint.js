5184ae157706bb49ae4395f5be815017e16d6f0f
关红福 <andreiguan@seeapp.com>
2019-09-11 14:50:22 +0800

    doc

--- entrypoint.js
+++ entrypoint.js
@@ -2,12 +2,69 @@ const axios = require('axios');
 const _ = require('lodash');
 const { argv } = require('yargs');
 
-const payload = {
-    msgtype: 'text',
-    text: {
-      content: `${process.env.GITHUB_REPOSITORY}/${process.env.GITHUB_WORKFLOW} triggered by ${process.env.GITHUB_ACTOR} (${process.env.GITHUB_EVENT_NAME})`,
+const payload = {};
+
+if (process.env.INPUT_MSGTYPE === 'text') {
+
+  payload.msgtype = process.env.INPUT_MSGTYPE;
+  payload.text = {
+    content: process.env.INPUT_CONTENT,
+  };
+
+  if (process.env.INPUT_MENTIONED_LIST) {
+    let mentioned_list;
+    try {
+      mentioned_list = JSON.parse(process.env.INPUT_MENTIONED_LIST);
+    } catch (error) {
+      mentioned_list = [];
     }
-};
+    payload.text.mentioned_list = mentioned_list;
+  }
+
+  if (process.env.INPUT_MENTIONED_MOBILE_LIST) {
+    let mentioned_mobile_list;
+    try {
+      mentioned_mobile_list = JSON.parse(process.env.INPUT_MENTIONED_MOBILE_LIST);
+    } catch (error) {
+      mentioned_mobile_list = [];
+    }
+    payload.text.mentioned_mobile_list = mentioned_mobile_list;
+  }
+
+}
+
+if (process.env.INPUT_MSGTYPE === 'markdown') {
+
+  payload.msgtype = process.env.INPUT_MSGTYPE;
+  payload.markdown = {
+    content: process.env.INPUT_CONTENT,
+  };
+
+}
+
+if (process.env.INPUT_MSGTYPE === 'image') {
+
+  payload.msgtype = process.env.INPUT_MSGTYPE;
+  payload.base64 = process.env.INPUT_BASE64;
+  payload.md5 = process.env.INPUT_MD5;
+
+}
+
+if (process.env.INPUT_MSGTYPE === 'news') {
+
+  payload.msgtype = process.env.INPUT_MSGTYPE;
+
+  let articles;
+  try {
+    articles = JSON.parse(process.env.INPUT_ARTICLES);
+  } catch (error) {
+    articles = [];
+  }
+  payload.news = {
+    articles,
+  };
+
+}
 
 console.log('The message content in JSON format...');
 console.log(JSON.stringify(payload));
